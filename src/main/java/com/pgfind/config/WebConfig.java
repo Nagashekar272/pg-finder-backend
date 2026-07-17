b42925433e371d80e1544aca828adfe782a1a95a
Nagashekar272 <nagashekarkummari830@gmail.com>
2026-07-17 17:53:04 +0530

    Update WebConfig.java

--- src/main/java/com/pgfind/config/WebConfig.java
+++ src/main/java/com/pgfind/config/WebConfig.java
@@ -10,7 +10,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://pg-finder-frontend-theta.vercel.app/")
+                .allowedOrigins(
+                        "https://pg-finder-frontend-theta.vercel.app",
+                        "https://pg-finder-frontend-theta.vercel.app/",
+                        "http://localhost:5173",
+                        "http://localhost:3000"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
